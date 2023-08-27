d39ab3094cb5ea4e4c6e687408765658bb0c5a9f
Sang Woo Yang (Dan) <79236624+IGhost-P@users.noreply.github.com>
2023-08-27 17:29:26 +0900

    feat(LottoTicket): add test code

--- jest/test/Model/LottoMachine.test.js
+++ jest/test/Model/LottoMachine.test.js
@@ -1,4 +1,5 @@
 import LottoMachine from "../../src/Model/LottoMachine";
+import LottoTicket from "../../src/Model/LottoTicket";
 
 describe("LottMachine", () => {
 	let lottoMachine;
@@ -8,9 +9,30 @@ describe("LottMachine", () => {
 
 	test("LottoTicket을 발급받을 수 있다", () => {
 		// when
-		const lottoTicket = lottoMachine.getLottoTicket();
+		const lottoTicket = lottoMachine.getLottoTicket({
+			lottoTicket: [1, 2, 3, 4, 5, 6],
+		});
 
 		// then
-		expect(lottoTicket).not.toBeUndefined();
+		expect(lottoTicket instanceof LottoTicket).toBeTruthy();
+	});
+
+	test("LottoTicket에 넘기는 숫자의 범위는 1~45", () => {
+		// when
+		const result = LottoMachine.generateNumbers();
+
+		// then
+		for (let num of result) {
+			expect(num).toBeGreaterThanOrEqual(1);
+			expect(num).toBeLessThanOrEqual(45);
+		}
+	});
+
+	test("LottoTicket에 넘기는 숫자는 중복되지 않는다", () => {
+		// when
+		const result = LottoMachine.generateNumbers();
+
+		// then
+		expect(new Set(result).size).toEqual(6);
 	});
 });
