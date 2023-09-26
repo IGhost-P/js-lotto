df4940e1f22873fd7f3c383ea2c2cdf0b82520f4
swYang <swYang@nexon.co.kr>
2023-09-26 20:53:34 +0900

    faet(frontend): test code 작성

--- jest/test/View/view.test.js
+++ jest/test/View/view.test.js
@@ -1,5 +1,55 @@
+import IOHandler from "../../../src/View/IOHandler";
+
 describe("view test", () => {
-	test("테스트 환경이 정상적으로 동작한다", () => {
-		expect(true).toBe(true);
-	});
+  let ioHandler;
+
+  beforeEach(() => {
+    ioHandler = new IOHandler();
+  });
+
+  test("로또 티켓을 구매할 수 있는 화면을 볼 수 있다", () => {
+    // when
+    const result = ioHandler.displayOutput("구매할 금액을 입력해 주세요.");
+    // then
+    expect(result).toBe("구매할 금액을 입력해 주세요.");
+  });
+
+  test("당첨 번호를 입력할 수 있는 화면을 볼 수 있다", () => {
+    // when
+    const result = ioHandler.displayOutput("당첨 번호를 입력해 주세요.");
+    // then
+    expect(result).toBe("당첨 번호를 입력해 주세요.");
+  });
+
+  test("보너스 볼을 입력할 수 있는 화면을 볼 수 있다", () => {
+    // when
+    const result = ioHandler.displayOutput("보너스 볼을 입력해 주세요.");
+    // then
+    expect(result).toBe("보너스 볼을 입력해 주세요.");
+  });
+
+  test("통계를 확인할 수 있는 화면을 볼 수 있다.", () => {
+    // given
+    const lottoStatistics = {
+      3: 1,
+      4: 0,
+      5: 0,
+      5.5: 0,
+      6: 0,
+      totalProfitRate: 62.5
+    };
+
+    // when
+    const result = ioHandler.displayOutput(lottoStatistics);
+
+    // then
+    expect(result).toBe(`당첨 통계
+      --------------------
+      3개 일치 (5,000원) - 1개
+      4개 일치 (50,000원) - 0개
+      5개 일치 (1,500,000원) - 0개
+      5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
+      6개 일치 (2,000,000,000원) - 0개
+      총 수익률은 62.5%입니다.`);
+  });
 });
---
+++ src/View/IOHandler.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+class IOHandler {
+  constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
+
+  getInput(question) {
+    return new Promise((resolve) => {
+      this.rl.question(question, (answer) => {
+        resolve(answer);
+      });
+    });
+  }
+
+  displayOutput(output) {
+    console.log(output);
+  }
+
+  close() {
+    this.rl.close();
+  }
+}
